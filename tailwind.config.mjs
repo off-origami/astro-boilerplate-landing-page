1687fc9f5f747ecf071cd035c7cefae2180df165
off-origami <baskakovihor@gmail.com>
2024-09-29 06:49:49 -0700

    updated tailwind theme with default themes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,34 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "",
+      md: "",
+      lg: "",
+      xl: ""
+    },
+    extend: {
+      colors: {
+        primary: "",
+        secondary: "",
+        accent: {
+          DEFAULT: "",
+          secondary: "",
+          tertiary: ""
+        },
+        background: {
+          primary: "",
+          secondary: ""
+        }
+      },
+      fontFamily: {
+        primary: ""
+      },
+      boxShadow: {
+        custom1: "",
+        custom2: ""
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
